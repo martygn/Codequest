0b4fd1002ca5bc8f057574f82709ac81ff47e032
martygn <martha.gom37@gmail.com>
2025-12-03 02:25:17 -0600

    cambios locales antes de actualizar

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,17 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use App\Http\Controllers\Auth\SocialAuthController;
+
+// Google OAuth
+Route::get('/auth/google', [SocialAuthController::class, 'redirectToGoogle'])
+    ->name('auth.google');
+Route::get('/auth/google/callback', [SocialAuthController::class, 'handleGoogleCallback']);
+
+// Facebook OAuth
+Route::get('/auth/facebook', [SocialAuthController::class, 'redirectToFacebook'])
+    ->name('auth.facebook');
+Route::get('/auth/facebook/callback', [SocialAuthController::class, 'handleFacebookCallback']);
 
 
 /*
